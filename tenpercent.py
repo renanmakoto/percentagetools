b7a6f77a03bb945422f0f23e40ce027475a70d3e
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-09-25 07:13:00 -0400

    Update tenpercent.py

--- tenpercent.py
+++ tenpercent.py
@@ -4,6 +4,7 @@ totalweeks = int(input('How many weeks? '))
 weeksprice = perweek * totalweeks
 
 print(f'${perweek:.2f} * {totalweeks} weeks = ${weeksprice:.2f}')
+
 tenpercentloa = (weeksprice * 10) / 100
 resttenpercent = weeksprice * 0.90
 print(f'The 10% of the total: ${tenpercentloa:.2f}')
